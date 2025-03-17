5ee0db6d8565158c08d08e1d9a1a67ebece47148
thegldstandard <adam@thegoldstandard.com>
2025-03-17 16:21:38 +0000

    Remove extra content and update price banner

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React from "react";
-import Prices from "./components/Prices"; // import the Prices component
+import Prices from "./components/prices"; // Make sure this matches your file name exactly
 
 function App() {
   return (
     <div>
-      {/* Render the Prices banner at the top */}
       <Prices />
-
-      {/* The rest of your application content goes here */}
-      <div style={{ marginTop: "60px" }}>
-        {/* 60px top margin so that your page content
-            doesn't hide behind the fixed Prices banner */}
-        <h1>Welcome to My Website</h1>
-        <p>Here’s some other content on the page...</p>
-      </div>
     </div>
   );
 }
